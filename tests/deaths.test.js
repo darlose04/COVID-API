c39b3fe4be978bb0076e6dd10d78cab38991859b
darlose04 <zsmith126028@gmail.com>
2020-05-02 20:50:11 -0500

    change url of route

--- tests/deaths.test.js
+++ tests/deaths.test.js
@@ -6,7 +6,7 @@ describe("test route for all death statistics", () => {
   test("all stats are returned as json", async () => {
     console.log("Entered test...");
     await api
-      .get("/api/coronadeaths")
+      .get("/api/usa/coronadeaths")
       .expect(200)
       .expect("Content-Type", /application\/json/);
   });
