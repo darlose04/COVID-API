19a834080234f05c047cad7b415b5ad2ac8cd2f6
darlose04 <zsmith126028@gmail.com>
2020-05-02 21:08:49 -0500

    add tests for cases

---
+++ tests/cases.test.js
@@ -0,0 +1,15 @@
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+
+describe("test route for all death statistics", () => {
+  test("all stats are returned as json", async (done) => {
+    console.log("Entered test...");
+    await api
+      .get("/api/usa/coronacases")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    done();
+  });
+});
