86996125335a8d72912e3814f10d630627a99dc2
darlose04 <zsmith126028@gmail.com>
2020-05-02 21:33:22 -0500

    add routes to get info from different states

--- routes/cases/casesRoutes.js
+++ routes/cases/casesRoutes.js
@@ -10,4 +10,13 @@ router.get("/", (req, res) => {
     .then((items) => res.json(items));
 });
 
+router.get("/states/:name", (req, res) => {
+  knex("usa")
+    .where({
+      State: req.params.name,
+    })
+    .innerJoin("cases", "usa.uid", "cases.uid")
+    .then((items) => res.json(items));
+});
+
 module.exports = router;
