7a7d282489995d7d08b6c73c6508a21d834c4d80
darlose04 <zsmith126028@gmail.com>
2020-05-02 20:31:06 -0500

    add test to check if death stats are returned as json

--- tests/deaths.test.js
+++ tests/deaths.test.js
@@ -1,3 +1,13 @@
 const supertest = require("supertest");
 const ap = require("../app");
 const api = supertest(app);
+
+describe("test route for all death statistics", () => {
+  test("all stats are returned as json", async () => {
+    console.log("Entered test...");
+    await api
+      .get("/api/coronadeaths")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+});
