9c9980afc309bb0e20691432ef06f4af204a6637
darlose04 <zsmith126028@gmail.com>
2020-05-02 20:46:31 -0500

    add route to get all death statistics

--- routes/deaths/deathRoutes.js
+++ routes/deaths/deathRoutes.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const knex = require("../../config/database");
+
+// get all death statistics
+router.get("/", (req, res) => {
+  knex
+    .from("usa")
+    .innerJoin("deaths", "usa.uid", "deaths.uid")
+    .then((items) => res.json(items));
+});
