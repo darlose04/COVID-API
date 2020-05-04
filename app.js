7c52c097c6fcf56320205fd39372096ce2e83507
darlose04 <zsmith126028@gmail.com>
2020-05-04 15:42:25 -0500

    remove unnecessary comment

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const compression = require("compression");
 
-// require db, will come from config directory
-
 const app = express();
 app.use(cors());
 app.use(compression());
