c85ab8bef543cf211725d799a6be9f7e8af57430
darlose04 <zsmith126028@gmail.com>
2020-05-11 11:33:59 -0500

    create route to get everything from daily reports table

---
+++ routes/dailyReports/dailyReportsRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../../config/database");
+
+// get all daily reports from table
+router.get("/dailyreport", (req, res) => {
+  knex
+    .select()
+    .table("daily_report")
+    .then((items) => res.json(items));
+});
+
+module.exports = router;
