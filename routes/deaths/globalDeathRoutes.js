04dc646f98dad56c1873b82c07f2f058feb45eaf
Zach Smith <zsmith126028@gmail.com>
2020-07-30 14:58:04 -0500

    add route to get death stats for specific countries

--- routes/deaths/globalDeathRoutes.js
+++ routes/deaths/globalDeathRoutes.js
@@ -10,4 +10,14 @@ router.get("/", (req, res) => {
     .then((items) => res.json(items));
 });
 
+// get death stats for a specific country
+router.get("/countries/:country", (req, res) => {
+  knex("global")
+    .where({
+      Country: req.params.country,
+    })
+    .innerJoin("global_deaths", "global.id", "global_deaths.id")
+    .then((items) => res.json(items));
+});
+
 module.exports = router;
